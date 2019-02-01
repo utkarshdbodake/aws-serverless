317c5e0df1028a6ad5dbce797ed8fbea7341431e
Utkarsh Bodake <utkarshdbodake@gmail.com>
2019-02-01 15:49:47 +0100

    Add java lambda implementation with no framework.

---
+++ aws-lambda-java-no-framework/src/main/java/com/serverless/PingHandler.java
@@ -0,0 +1,28 @@
+package com.serverless;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class PingHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private static final Logger LOG = Logger.getLogger(PingHandler.class);
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("received: " + input);
+        Response responseBody = new Response("Pong");
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-Powered-By", "AWS Lambda & Serverless");
+        headers.put("Content-Type", "application/json");
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(responseBody)
+                .setHeaders(headers)
+                .build();
+    }
+}
